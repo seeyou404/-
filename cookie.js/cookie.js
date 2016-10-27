d30916aadda722ac30649bd4e0b2322c7c2b43a0
jserman <1457358080@qq.com>
2016-10-28 00:33:54 +0800

    添加源文件注释

--- cookie.js/cookie.js
+++ cookie.js/cookie.js
@@ -1,30 +1,41 @@
 // Copyright (c) 2015 Florian Hartmann, https://github.com/florian https://github.com/florian/cookie.js
-
+/*
+	cookie所使用的所有的方法都是静态方法  当直接调用cookie这个函数的时候 默认以get的方式进行处理
+*/
 !function (document, undefined) {
 
 	var cookie = function () {
+		/*
+			这个方法其实就是cookie.get的一种简写形式
+		*/
 		return cookie.get.apply(cookie, arguments);
 	};
 
 	var utils = cookie.utils =  {
-
+		/*
+			cookie.js内部使用的工具方法
+		*/
 		// Is the given value an array? Use ES5 Array.isArray if it's available.
 		isArray: Array.isArray || function (value) {
+			//判断是不是数组的方法
 			return Object.prototype.toString.call(value) === '[object Array]';
 		},
 
 		// Is the given value a plain object / an object whose constructor is `Object`?
 		isPlainObject: function (value) {
+			//判断是不是json对象的方法
 			return !!value && Object.prototype.toString.call(value) === '[object Object]';
 		},
 
 		// Convert an array-like object to an array – for example `arguments`.
 		toArray: function (value) {
+			//将类数组转换成数组的方法
 			return Array.prototype.slice.call(value);
 		},
 
 		// Get the keys of an object. Use ES5 Object.keys if it's available.
 		getKeys: Object.keys || function (obj) {
+      //获取对象属性的方法
 			var keys = [],
 				 key = '';
 			for (key in obj) {
@@ -36,12 +47,16 @@
 		// Unlike JavaScript's built-in escape functions, this method
 		// only escapes characters that are not allowed in cookies.
 		encode: function (value) {
+			//转义函数  这个函数并不是将所有的字符都进行转义 而是将不能在cookie中存在的字符进行转义
+			// 从以下的函数可以知道在cookie中不能存在的字符串主要有:,---;---"---\---=---空格---%
 			return String(value).replace(/[,;"\\=\s%]/g, function (character) {
+				// 这里的character就是和正则表达式匹配的部分
 				return encodeURIComponent(character);
 			});
 		},
 
 		decode: function (value) {
+			// 对转义之后的字符进行解义
 			return decodeURIComponent(value);
 		},
 
@@ -52,38 +67,68 @@
 
 	};
 
+	// 设置cookie类的默认对象
 	cookie.defaults = {};
-
+	// 设置默认的过期时间的基数值--1天
 	cookie.expiresMultiplier = 60 * 60 * 24;
 
+	// 设置cookie的方法
 	cookie.set = function (key, value, options) {
+		/*
+			这个方法是设置cookie的方法  该方法接收三个参数  第一个参数可以是对象或者是字符串  如果是对象
+			则对象中的key就是cookie的name value就是cookie对应的value
+		*/
 		if (utils.isPlainObject(key)) {
-			// `key` contains an object with keys and values for cookies, `value` contains the options object.
 
+			// `key` contains an object with keys and values for cookies, `value` contains the options object.
+			/*
+				如果key是一个对象的话 则第二个参数就不再表示value了  就是表示的是选项对象
+			*/
 			for (var k in key) {
 				if (key.hasOwnProperty(k)) this.set(k, key[k], value);
 			}
 		} else {
+			// 为options设置默认值  如果options是一个对象的话  则就取这个对象  否则就把这个对象当作是设置过期时间的值
 			options = utils.isPlainObject(options) ? options : { expires: options };
 
 			// Empty string for session cookies.
+			// 这里主要是获取到expires的值 如果想要为这个传值  默认单位是天
+			/*
+				这里需要知道expires的来源有几种：有两种
+				主要取决于optioans的数据类型  如果options是一个json对象的话  则expires是作为这个对象的一个属性存在的
+				 如果options是非对象的话  则这个值就表示expires的值
+
+				 不管怎样  这两种情况都会将expires转换成一个json对象的属性
+			*/
+
+			/*
+				这个主要是获取到expires的值  从以下的表达式  可以看出expires的值的优先级是 指定的 大于 默认的  大于 空
+			*/
 			var expires = options.expires !== undefined ? options.expires : (this.defaults.expires || ''),
-			    expiresType = typeof(expires);
-
+					// 获取数据类型
+					expiresType = typeof(expires);
+			// 针对expires是字符串的操作--这显示expires是字符串的时候  可以传入任何能够被new Date()识别的字符串
 			if (expiresType === 'string' && expires !== '') expires = new Date(expires);
+			// 针对expires是数字的操作--默认单位是天
 			else if (expiresType === 'number') expires = new Date(+new Date + 1000 * this.expiresMultiplier * expires); // This is needed because IE does not support the `max-age` cookie attribute.
-
+			// 这个主要是对expires进行标准化
+			/*
+				这里涉及到了cookie的一个基本设置选项：expires这个选项用于指定cookie的过期时间
+			*/
 			if (expires !== '' && 'toGMTString' in expires) expires = ';expires=' + expires.toGMTString();
 
+			// 这里主要是设置cookie的path
 			var path = options.path || this.defaults.path;
 			path = path ? ';path=' + path : '';
 
+			// 这里主要是设置cookie的domain
 			var domain = options.domain || this.defaults.domain;
 			domain = domain ? ';domain=' + domain : '';
 
+			// 这里主要是设置cookie的secure
 			var secure = options.secure || this.defaults.secure ? ';secure' : '';
 			if (options.secure === false) secure = '';
-
+			// 设置cookie--对值进行转义
 			document.cookie = utils.encode(key) + '=' + utils.encode(value) + expires + path + domain + secure;
 		}
 
@@ -102,9 +147,11 @@
 	},
 
 	cookie.remove = function (keys) {
+		// 这里先判断一下这个参数是不是数组  是数组的刷 就可以一次性的移除多个  不是的话  就先转成数组再操作
 		keys = utils.isArray(keys) ? keys : utils.toArray(arguments);
 
 		for (var i = 0, l = keys.length; i < l; i++) {
+			// 这是将cookie清除的最便捷的方法  只需要将它的过期时间设为过去的哪个时刻就行了
 			this.set(keys[i], '', -1);
 		}
 
@@ -115,6 +162,7 @@
 		if (!options) return this.remove(keys);
 
 		keys = utils.isArray(keys) ? keys : [keys];
+		// 同样的原理 将过期时间设置为过去的时间
 		options.expire = -1;
 
 		for (var i = 0, l = keys.length; i < l; i++) {
@@ -125,6 +173,7 @@
 	};
 
 	cookie.empty = function () {
+		// 先获取到所有的cookie
 		return this.remove(utils.getKeys(this.all()));
 	};
 
@@ -132,6 +181,7 @@
 		var cookies = this.all();
 
 		if (utils.isArray(keys)) {
+			// 是数组的话 就同时返回多个值 是一个对象
 			var result = {};
 
 			for (var i = 0, l = keys.length; i < l; i++) {
@@ -140,11 +190,12 @@
 			}
 
 			return result;
-
+			// 返回对应的cookie
 		} else return utils.retrieve(cookies[keys], fallback);
 	};
 
 	cookie.all = function () {
+		// 获取所有的cookie 存在一个数组中
 		if (document.cookie === '') return {};
 
 		var cookies = document.cookie.split('; '),
@@ -161,8 +212,9 @@
 	};
 
 	cookie.enabled = function () {
+		// navigator.cookieEnabled属性可返回一个布尔值,如果浏览器启用了 cookie,则返回true
 		if (navigator.cookieEnabled) return true;
-
+		// 调用cookie的方法  判断一下
 		var ret = cookie.set('_', '_').get('_') === '_';
 		cookie.remove('_');
 		return ret;
